a8790af81f7330d233bbf3333fcbe590e133f1fe
Marcel Martinici <sirmarcus1993@gmail.com>
2014-10-23 12:32:40 +0300

    Create model Author

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022115333) do
+ActiveRecord::Schema.define(version: 20141023092903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.integer  "books_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authors", ["books_id"], name: "index_authors_on_books_id", using: :btree
 
   create_table "books", force: true do |t|
     t.string   "title"
