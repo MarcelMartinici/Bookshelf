6e1e7792781de5cc3407b54786b5855bb4092b8e
Marcel Martinici <sirmarcus1993@gmail.com>
2014-10-31 14:27:59 +0200

    Navbar

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ActiveRecord::Base
   # author_id
   # author_id=  	
   validates :title, presence: true,length: { minimum: 3 }
+  validates :author_id, presence: true
   validates :year, presence: true,length: { maximum: 4 }
   has_attached_file :cover, :styles => { :medium => "640x480>"}, :default_url => "missing.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
