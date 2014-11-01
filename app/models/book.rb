4b6d105b40c24aed95c4effc48b5509a5536194d
Marcel Martinici <sirmarcus1993@gmail.com>
2014-11-01 10:47:13 +0200

    Search authors

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,8 +6,12 @@ class Book < ActiveRecord::Base
   validates :year, presence: true,length: { maximum: 4 }
   has_attached_file :cover, :styles => { :medium => "640x480>"}, :default_url => "missing.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
-  
-  def self.search(query)
-    where("title like ?", "%#{query}%")
-  end
+ 
+  scope :search, -> term {
+    if term.present?
+      where('title ILIKE :term OR description ILIKE :term OR authors.name ILIKE :term', term: "%#{term}%").
+      eager_load(:author)
+    end
+  }  
+
 end
