18f0bbe819e6bacf54c31862b6d6178a25d33718
Marcel Martinici <sirmarcus1993@gmail.com>
2014-11-05 15:37:37 +0200

    Some CSS

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,13 @@ class Book < ActiveRecord::Base
   belongs_to :user 	
   has_many :book_cathegory
   has_many :cathegory, :through => :book_cathegory 
+
   ratyrate_rateable 'book_rating'
+
   validates :title, presence: true,length: { minimum: 3 }
+  validates :author_id, presence: true
   validates :year, presence: true,length: { maximum: 4 }
+
   has_attached_file :cover, :styles => { :medium => "640x480>"}, :default_url => "missing.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
  
