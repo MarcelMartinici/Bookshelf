b0b939248d0c0840fe897c023dd553577eed6049
Marcel Martinici <sirmarcus1993@gmail.com>
2014-11-06 15:21:05 +0200

    Added test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'user@gmail.com', 
+             password: '123456789', 
+             password_confirmation: '123456789')
+
+Book.create(title: 'Amintiri din copilarie'
+             author_id: '1'
+             year: '1850')
+
+Book.create(title: 'Dracula'
+             author_id: '1'
+             year: '1900')
+
+Book.create(title: 'Steve Jobs'
+             author_id: '1'
+             year: '2013')
