de3459c1a77ed58b8c787ca6957672de9fcff6c5
Marcel Martinici <sirmarcus1993@gmail.com>
2014-11-06 15:38:11 +0200

    Added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
-  root 'books#index'
+  root 'welcome#index'
   devise_for :users
   resources :books 
   resources :author
